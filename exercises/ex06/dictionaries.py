fbaf6cc691798aa86822b700ccfdbf3bd681e79d
Alexandra Wrenn <alwrenn@ad.unc.edu>
2021-10-26 14:33:58 -0400

    ex06, notebook lessons

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,42 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730384411"
 
-# Define your functions below
+
+def invert(a: dict[str, str]) -> dict[str, str]:
+    """Invert a dictionary."""
+    result: dict[str, str] = {}
+    for item in a:
+        if a[item] in result:
+            raise KeyError("Repeated keys")
+        result[a[item]] = item
+    return result
+
+
+def favorite_color(c: dict[str, str]) -> str:
+    """Return the common favorite color."""
+    counter: dict[str, int] = {}
+    result: str = ""
+    big: int = 0
+    for item in c:
+        key: str = c[item]
+        if key in counter:
+            counter[key] += 1
+        else:
+            counter[key] = 1
+    for key in counter:
+        if counter[key] > big:
+            big = counter[key]
+            result = key
+    return result 
+
+
+def count(d: list[str]) -> dict[str, int]:
+    """Count the appearances of a value in a list."""
+    result: dict[str, int] = {}
+    for item in d:
+        if item in result:
+            result[item] += 1
+        else:
+            result[item] = 1
+    return result
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,62 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
+import pytest
 
-__author__ = "123456789"
+__author__ = "730384411"
+
+
+def test_invert_edge() -> None:
+    """Testing invert with an edge case."""
+    a: dict[str, str] = {'kris': 'jordan', 'michael': 'jordan'}
+    with pytest.raises(KeyError): 
+        invert(a)
+
+
+def test_invert_single() -> None:
+    """Testing invert with a single case."""
+    a: dict[str, str] = {'cat': 'apple'}
+    assert invert(a) == {'apple': 'cat'}
+
+
+def test_invert_multiple() -> None:
+    """Testing invert with multiple cases."""
+    a: dict[str, str] = {'z': 'a', 'y': 'b', 'x': 'c'}
+    assert invert(a) == {'a': 'z', 'b': 'y', 'c': 'x'}
+
+
+def test_favorite_color_same() -> None:
+    """Testing favorite_color with all same color."""
+    c: dict[str, str] = {'ally': 'blue', 'ann': 'blue', 'evan': 'blue'}
+    assert favorite_color(c) == 'blue'
+
+
+def test_favorite_color_tie() -> None:
+    """Testing favorite_color with a tie."""
+    c: dict[str, str] = {'ally': 'blue', 'ann': 'blue', 'jake': 'green', 'chloe': 'green'}
+    assert favorite_color(c) == 'blue'
+
+
+def test_favorite_color_norm() -> None:
+    """Testing favorite_color with a normal case."""
+    c: dict[str, str] = {'mark': 'yellow', 'ally': 'blue', 'ann': 'blue'}
+    assert favorite_color(c) == 'blue'
+
+
+def test_count_edge() -> None:
+    """Testing count with an edge case."""
+    d: list[str] = ['blue', 'blue', 'blue']
+    assert count(d) == {'blue': 3}
+
+
+def test_count_two() -> None:
+    """Testing count with two items."""
+    d: list[str] = ['blue', 'green']
+    assert count(d) == {'blue': 1, 'green': 1}
+
+
+def test_count_three() -> None:
+    """Testing count with three items."""
+    d: list[str] = ['blue', 'green', 'yellow']
+    assert count(d) == {'blue': 1, 'green': 1, 'yellow': 1}
